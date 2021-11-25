3a9b09d82f69c3eab332aae83239d5fc328ad592
Emily Morgan <emily189morgan@hotmail.com>
2021-11-24 22:24:28 -0500

    adding style and database

--- includes/sql/db_miniapp.sql
+++ includes/sql/db_miniapp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 25, 2021 at 02:38 AM
+-- Generation Time: Nov 25, 2021 at 03:23 AM
 -- Server version: 5.7.31
 -- PHP Version: 7.3.21
 
@@ -36,6 +36,7 @@ CREATE TABLE IF NOT EXISTS `tbl_cardata` (
   `Finance` varchar(15) NOT NULL,
   `Engine` varchar(100) NOT NULL,
   `FullName` varchar(50) NOT NULL,
+  `Price` varchar(15) NOT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
@@ -43,11 +44,11 @@ CREATE TABLE IF NOT EXISTS `tbl_cardata` (
 -- Dumping data for table `tbl_cardata`
 --
 
-INSERT INTO `tbl_cardata` (`ID`, `MainPic`, `Name`, `Lease`, `Finance`, `Engine`, `FullName`) VALUES
-(1, 'cooper.png', 'Cooper', '3.99% ', '2.99%', '3 Cylinder 12 Valve Twin Power Turbo', '2022 Mini Cooper 3 Door'),
-(2, 'CooperS.png', 'Cooper S', '3.99%', '2.99%', '4 Cylinder 16 Valve Twin Power Turbo', '2022 Mini Cooper S 3 Door'),
-(3, 'CooperSE3Door.png', 'Cooper SE 3 Door', '4.99%', '2.99%', 'Fully Electric Motor', '2022 Mini Cooper SE 3 Door'),
-(4, 'johnCooperWorks.png', 'John Cooper Works', '3.99%', '2.99%', '4 Cylinder 16 Valve Twin Power Turbo', '2022 Mini John Cooper Works 3 Door');
+INSERT INTO `tbl_cardata` (`ID`, `MainPic`, `Name`, `Lease`, `Finance`, `Engine`, `FullName`, `Price`) VALUES
+(1, 'cooper.png', 'Cooper', '3.99% ', '2.99%', '3 Cylinder 12 Valve Twin Power Turbo', '2022 Mini Cooper 3 Door', '$27,456'),
+(2, 'CooperS.png', 'Cooper S', '3.99%', '2.99%', '4 Cylinder 16 Valve Twin Power Turbo', '2022 Mini Cooper S 3 Door', '$38,956'),
+(3, 'CooperSE3Door.png', 'Cooper SE 3 Door', '4.99%', '2.99%', 'Fully Electric Motor', '2022 Mini Cooper SE 3 Door', '$38,906'),
+(4, 'johnCooperWorks.png', 'John Cooper Works', '3.99%', '2.99%', '4 Cylinder 16 Valve Twin Power Turbo', '2022 Mini John Cooper Works 3 Door', '$31,756');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
